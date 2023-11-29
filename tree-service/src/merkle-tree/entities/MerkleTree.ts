2f57c04ecf57e47a481794995bf40334269a0718
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-11-29 09:09:24 +0000

    prepare proove serialization

--- tree-service/src/merkle-tree/entities/MerkleTree.ts
+++ tree-service/src/merkle-tree/entities/MerkleTree.ts
@@ -5,11 +5,11 @@ const MERKLE_ROOT_INDEX = 1n;
 
 export default class MerkleTree<T extends MerkleTreeNode> {
 
-    depth: bigint;
+    depth: number;
     merkleTree: Map<bigint, Buffer> = new Map();
     leaves: T[] = [];
 
-    constructor(depth = 32n) {
+    constructor(depth = 32) {
         this.depth = depth;
     }
 
@@ -21,14 +21,19 @@ export default class MerkleTree<T extends MerkleTreeNode> {
         return this.getRootHash().toString('hex');
     }
 
-    appendLeaf(merkleTreeNode: T) {
-        this.leaves.push(merkleTreeNode);
-        this.updateMerkleTree(this.leaves.length - 1);
+    getSiblingIndex(i: bigint): bigint {
+        return i ^ 1n;
     }
 
-    updateMerkleTree(leafIndex: number) {
-        const nodeHash = this.leaves[leafIndex].hash;
-        const merkleLeafIndex = (MERKLE_ROOT_INDEX << this.depth) + BigInt(leafIndex);
+    appendLeaf(merkleTreeNode: T) {
+        merkleTreeNode.leafIndex = BigInt(this.leaves.length);
+        this.leaves.push(merkleTreeNode);
+        this.updateMerkleTree(merkleTreeNode);
+    }
+
+    updateMerkleTree(merkleTreeNode: T) {
+        const nodeHash = merkleTreeNode.hash;
+        const merkleLeafIndex = (MERKLE_ROOT_INDEX << (BigInt(this.depth - 1))) + BigInt(merkleTreeNode.leafIndex);
 
         this.merkleTree.set(merkleLeafIndex, nodeHash);
 
@@ -50,6 +55,43 @@ export default class MerkleTree<T extends MerkleTreeNode> {
 
             currentMerkleLeafIndex = parentMerkleLeafIndex;
         }
+    }
+
+    getRootHashAsUint8Array(): Uint8Array {
+        return Uint8Array.from(this.getRootHash());
+    }
+
+    getPathToRoot(merkleTreeNode: T): Uint8Array {
+        const path = new Uint8Array(this.depth);
+        const leafIndex = merkleTreeNode.leafIndex;
+
+        // last index is always 0
+        path[this.depth - 1] = 0;
+        for (let i = path.length - 1; i-- > 0;) {
+            const bigI = BigInt(i);
+            path[i] = Number((leafIndex >> bigI) & 1n);
+        }
+
+        return path;
+    }
+
+    getPathToRootSiblingsHashes(merkleTreeNode: T): Uint8Array[] {
+        const result: Uint8Array[] = [];
+        let leafIndex = merkleTreeNode.leafIndex;
+
+        while (leafIndex > MERKLE_ROOT_INDEX) {
+            const siblingIndex = this.getSiblingIndex(leafIndex);
+            const siblingHash = this.merkleTree.get(siblingIndex);
+            if (siblingHash === undefined) {
+                console.log('unable to find sibling with index', siblingIndex, 'from index', leafIndex);
+                process.exit(1);
+            }
+            result.push(Uint8Array.from(siblingHash));
+
+            leafIndex >>= 1n;
+        }
+
+        return result;
     }
 
 }
--- tree-service/src/merkle-tree/entities/MerkleTreeNode.ts
+++ tree-service/src/merkle-tree/entities/MerkleTreeNode.ts
@@ -1,6 +1,7 @@
 export default class MerkleTreeNode {
 
     hash: Buffer = Buffer.alloc(0);
+    leafIndex: bigint = -1n;
 
     getHashAsString(): string {
         return this.hash.toString('hex');
